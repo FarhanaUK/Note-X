db1cae65e99e3a42d7ce2d4f5356f209d15a795b
Farhana Aktar <farhanaaktar@live.co.uk>
2024-12-20 14:51:31 +0000

    version 1.03 done

--- src/component/Download.jsx
+++ src/component/Download.jsx
@@ -7,7 +7,7 @@ function Download({downloadNotes, state, dispatch}) {
 
 
 return(
-<div className="absolute top-1 right-1 flex justify-between items-center mb-4 mr-2">
+<div className="absolute top-2 right-1 flex justify-between items-center mb-4 mr-2">
 <button
   onClick={() => downloadNotes(state, dispatch)}
   className=" text-sm border border-gray-300 rounded dark:bg-input-dark-bg"
